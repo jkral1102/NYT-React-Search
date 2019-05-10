189a66fc9e8e991ee5f211382f8bd7e2cdcd4c53
jkral1102 <jkral1101@gmail.com>
2019-05-09 23:37:04 -0500

    dumb heroku

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const articlesController = require("../../controllers/articlesController");
+const articlesController = require("../../Controllers/articlesController");
 
 // Matches with "/api/articles"
 router.route("/")
